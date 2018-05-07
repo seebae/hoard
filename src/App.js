c97e30cc075759119ab683be2a8cc489f6f62b5c
Eric Gibb <ericgibb@gmail.com>
2018-05-07 02:07:17 -0400

    [NEW] handle deep linking from root app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
+import {
+ UrbanAirship,
+} from 'urbanairship-react-native'
+import {
+  Alert,
+  AppRegistry,
+  Button,
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  ListView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
 import NavigatorService from 'lib/navigator';
 import configureStore from './configureStore';
 /* import HockeyApp from 'react-native-hockeyapp';*/
@@ -32,6 +54,40 @@ const RoutingStack = StackNavigator(
 
 export let navigatorRef;
 
+/*
+  Example usage:
+  handleDeepLink('org.reactjs.native.example.Hoard://confirm_transaction/?params={"tx":"123","uid":"12"}')
+ */
+
+function handleDeepLink(deepLink){
+  const SCHEME = 'org.reactjs.native.example.Hoard://';
+  let paths, params;
+
+  let link = deepLink.replace(SCHEME, "");
+
+  link = link.split('/');
+  alert(`Deep Link: ${link[0]}`)
+
+  if (link[link.length-1].includes('?params=')){
+    params = link[link.length-1];
+    params = params.replace('?params=', "")
+    alert(`Params: ${params}`)
+  }
+
+  if (link[0] === 'confirm_transaction') {
+     NavigatorService.navigateDeep([
+      { routeName: "Wallet" }, { routeName: "Confirm", params: JSON.parse(params) }
+    ]);
+  }
+  else{
+    // TODO handle default or unhandled deeplinks
+    NavigatorService.navigate('Dashboard');
+  }
+
+
+}
+
+
 export default class App extends React.Component {
   componentWillMount() {
     /* HockeyApp.configure(Config.HOCKEYAPP_API_KEY, true);*/
@@ -42,6 +98,37 @@ export default class App extends React.Component {
     /* HockeyApp.checkForUpdate();*/
     store.dispatch({ type: INIT_REQUESTING });
     SplashScreen.hide();
+  }
+
+  componentWillMount() {
+    UrbanAirship.getChannelId().then((channelId) => {
+      // this.setState({channelId:channelId})
+    });
+
+    UrbanAirship.addListener("notificationResponse", (response) => {
+      console.log('notificationResponse:', JSON.stringify(response));
+      alert("notificationResponse: " + response.notification.alert);
+    });
+
+    UrbanAirship.addListener("pushReceived", (notification) => {
+      console.log('pushReceived:', JSON.stringify(notification));
+      alert("pushReceived: " + notification.alert);
+    });
+
+    UrbanAirship.addListener("deepLink", (event) => {
+      console.log('deepLink:', JSON.stringify(event));
+      handleDeepLink(event.deepLink)
+    });
+
+    UrbanAirship.addListener("registration", (event) => {
+      console.log('registration:', JSON.stringify(event));
+      // this.state.channelId = event.channelId;
+      // this.setState(this.state);
+    });
+
+    UrbanAirship.addListener("notificationOptInStatus", (event) => {
+      console.log('notificationOptInStatus:', JSON.stringify(event));
+    });
   }
 
   render() {
